adc87720c445ce74388e3952fc687962de3dc978
Liam Douglas <liam.douglas23@gmail.com>
2024-12-17 02:23:43 +0000

    day 11 (finished)

--- aoc/src/problem11.rs
+++ aoc/src/problem11.rs
@@ -1,4 +1,4 @@
-use std::vec;
+use std::{collections::HashMap, vec};
 
 #[derive(Debug)]
 pub struct Input {
@@ -54,31 +54,43 @@ pub fn solution(input: Input) -> usize {
     solve(input, 25)
 }
 
-pub fn solution_part_two(input: Input) -> usize {
-    let mut result = 0;
+type Length = usize;
+type Stone = usize;
 
-    for stone in input.stones {
-        result += blink(stone, 74);
-    }
+/*
+    The caches store the final length that a given stone adds to the sequence after the specified number of steps.
 
-    result
-}
+    For example, depth_cache[2] stores the map for depth 3, so each key in the map has a value that indicates the length by which the sequence will increase when that key is observed with 3 steps remaining.
+*/
 
-fn blink(stone: usize, count: usize) -> usize {
-    let next = step(stone);
+type Cache = HashMap<Stone, Length>;
+type Caches = Vec<Cache>;
 
-    if count == 0 {
-        next.len()
-    } else {
-        if next.len() == 1 {
-            blink(next[0], count - 1)
-        } else {
-            blink(next[0], count - 1) + blink(next[1], count - 1)
+fn expand(stone: Stone, depth: usize, caches: &mut Caches) -> Length {
+    let cache = &caches[depth];
+
+    match cache.get(&stone) {
+        Some(length) => *length,
+        None => {
+            let next = step(stone);
+
+            let result = if depth == 0 {
+                next.len()
+            } else {
+                if next.len() == 1 {
+                    expand(next[0], depth - 1, caches)
+                } else {
+                    expand(next[0], depth - 1, caches) + expand(next[1], depth - 1, caches)
+                }
+            };
+
+            caches[depth].insert(stone, result);
+            return result;
         }
     }
 }
 
-fn step(stone: usize) -> Vec<usize> {
+fn step(stone: Stone) -> Vec<Stone> {
     if stone == 0 {
         return vec![1];
     } else if stone.ilog10() % 2 == 1 {
@@ -96,6 +108,24 @@ fn step(stone: usize) -> Vec<usize> {
     } else {
         return vec![stone * 2024];
     }
+}
+
+pub fn solution_part_two(input: Input) -> usize {
+    let mut caches: Caches = vec![];
+    let mut result = 0;
+
+    for i in 0..75 {
+        caches.push(HashMap::new());
+
+        let mut lengths = 0;
+
+        for stone in &input.stones {
+            lengths += expand(*stone, i, &mut caches);
+        }
+
+        result = lengths;
+    }
+    result
 }
 
 #[cfg(test)]
